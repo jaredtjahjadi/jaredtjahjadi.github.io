5cba6263f2e3fc93f6222afd153a0d685199fdaf
JareBear16 <jaredtjahjadi@gmail.com>
2023-07-29 16:28:10 -0400

    Adjusted styling for about and resume sections

--- src/About.js
+++ src/About.js
@@ -10,7 +10,7 @@ export default function About() {
                     <img src={AboutImage} alt="Jared Tjahjadi" />
                 </div>
                 <div id="about-text">
-                    My name is Jared Tjahjadi. I am an undergraduate student from Queens, NY, currently studying Computer Science at Stony Brook University.
+                    My name is Jared Tjahjadi. I am a senior undergraduate student from Queens, NY, currently studying Computer Science at Stony Brook University. My main coding languages are Java, JavaScript, and Python.
                     <br /><br />
                     In my free time, I enjoy listening to music, going to the gym, and taking walks.
                 </div>
--- src/ResumeCard.js
+++ src/ResumeCard.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
-export default function ResumeCard({name, date, addl_text, listItem1, listItem2, listItem3}) {
+export default function ResumeCard({name, date, addl_text, courseworkTitle, courseworkContent, listItem1, listItem2, listItem3}) {
     return (
         <div className="resume-card">
             <div className="name-date">
                 <p className="name">{name}</p>
                 <p className="date">{date}</p>
-                {addl_text}
+                <div className='addl-txt'>{addl_text ? addl_text : null}</div>
+                <div className="coursework-title">{courseworkTitle ? courseworkTitle : null}</div>
+                <div className="coursework-content">{courseworkContent ? courseworkContent : null}</div>
             </div>
-            <ul>
+            {listItem1 ? <ul>
                 {listItem1 ? <li>{listItem1}</li> : null }
                 {listItem2 ? <li>{listItem2}</li> : null }
                 {listItem3 ? <li>{listItem3}</li> : null }
-            </ul>
+            </ul> : null}
         </div>
     )
 }
