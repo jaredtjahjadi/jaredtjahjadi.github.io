afde28a90b5a023d56c315628f81a7753344cca9
Jared Tjahjadi <jared.tjahjadi@stonybrook.edu>
2023-08-02 01:00:50 -0400

    Removed attempt at responsive navbar while I try to figure it out

--- src/Navbar.js
+++ src/Navbar.js
@@ -11,11 +11,6 @@ export default function Navbar() {
                 <li><Link to="projects" spy smooth duration={500}>Projects</Link></li>
                 <li><Link to="contact" spy smooth duration={500}>Contact</Link></li>
             </ul>
-            <button className="hamburger">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="white">
-                    <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clipRule="evenodd" />
-                </svg>
-            </button>
         </nav>
     )
 }
