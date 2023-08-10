905d5d7ea266a57f5b855d1f61b31b8387f340c4
Jared Tjahjadi <jared.tjahjadi@stonybrook.edu>
2023-08-10 14:35:39 -0400

    Navbar now changes color, fixed index.css and replaced it with .scss

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,8 +1,19 @@
+import { useState } from 'react';
 import { Link } from 'react-scroll';
 
 export default function Navbar() {
+    const [navbar, setNavbar] = useState(false);
+    const changeBG = () => {
+        if(window.scrollY >= 60)
+            setNavbar(true);
+        else
+            setNavbar(false);
+    }
+
+    window.addEventListener('scroll', changeBG);
+
     return (
-        <nav>
+        <nav className={navbar ? 'active' : ''}>
             <ul>
                 <li><Link to="hero" spy smooth duration={500}>Home</Link></li>
                 <li><Link to="about" spy smooth duration={500}>About</Link></li>
